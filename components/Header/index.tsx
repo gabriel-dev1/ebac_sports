2b214962b0157fb56ba1f2b4c82f2142114a7165
Gabriel <127807744+gabriel-dev1@users.noreply.github.com>
2023-06-05 20:09:23 -0300

    Upload do exercício

---
+++ components/Header/index.tsx
@@ -0,0 +1,33 @@
+import { useSelector } from 'react-redux'
+
+import * as S from './styles'
+
+import cesta from '../../assets/cesta.png'
+import { paraReal } from '../Produto'
+
+import { RootReducer } from '../../store'
+
+const Header = () => {
+  const carrinho = useSelector((state: RootReducer) => state.carrinho.itens)
+  const favoritos = useSelector((state: RootReducer) => state.favoritos.itens)
+
+  const valorTotal = carrinho.reduce((acc, item) => {
+    acc += item.preco
+    return acc
+  }, 0)
+
+  return (
+    <S.Header>
+      <h1>EBAC Sports</h1>
+      <div>
+        <span>{favoritos.length} favoritos</span>
+        <img src={cesta} />
+        <span>
+          {carrinho.length} itens, valor total: {paraReal(valorTotal)}
+        </span>
+      </div>
+    </S.Header>
+  )
+}
+
+export default Header
